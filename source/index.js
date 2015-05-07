6452221df64915c51d8dcde218e782017de7b53a
Daniel Demmel <dain@danieldemmel.me>
2015-05-07 19:42:31 +0100

    First interactions and transitions on index page.

--- source/index.js
+++ source/index.js
@@ -1,23 +1,43 @@
 var gsap = require('gsap');
 var page = require('page');
 var hbtemplates = require('./templates.js');
+var indexData = null;
+var overlay = document.querySelector('#overlay');
 
-console.log('i\'m alive !');
+console.log('I\'m alive!');
 
-function loadJSON(file, callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
+page('/', function(context){
+  console.log(context);
 
-loadJSON('data/blog.json', function(response) {
-    var actual_JSON = JSON.parse(response);
-    document.querySelector('.blog__inner').insertAdjacentHTML( 'beforeend', hbtemplates.templates.blocks(actual_JSON) );
+  if(context.hash === '') {
+    if (!indexData) {
+      loadJSON('data/index.json', function(response) {
+          indexData = JSON.parse(response);
+          document.querySelector('.blog__inner').insertAdjacentHTML('beforeend', hbtemplates.templates.blocks(indexData));
+      });
+    } else {
+      TweenLite.to(overlay, 0.5, {autoAlpha:0, ease:Circ.easeIn});
+    }
+  } else if(context.hash === 'what-we-do') {
+    TweenLite.from(overlay, 0.7, {left:'300px', right:'300px', top:'400px', height:'310px', ease:Circ.easeOut});
+    TweenLite.to(overlay, 0.5, {autoAlpha:1, ease:Circ.easeOut});
+  }
 });
+
+page.start();
+
+
+
+// helper for blocks on home page
+function loadJSON(file, callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+  xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+        callback(xobj.responseText);
+      }
+  };
+  xobj.send(null);
+}
