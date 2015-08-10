d40bc9ca5100a47afca6e678edad51af1c65b047
Phil Linnell <phil@ustwo.com>
2015-08-10 17:54:00 +0100

    join us hero style

--- src/app/templates/page-join-us.js
+++ src/app/templates/page-join-us.js
@@ -4,16 +4,49 @@ import React from 'react';
 import find from 'lodash/collection/find';
 import get from 'lodash/object/get';
 
+import DownChevron from 'elements/down-chevron';
+
+
 
 export default class PageJoinUs extends React.Component {
   render() {
-    const pageData = this.props.page;
+    // const pageData = this.props.page;
+    const pageData = {
+      "page_builder": [{
+        "attr": {
+          "heading": {
+            "value": "Do the best work of your life"
+          }
+        }
+      }]
+    };
+
     return (
       <article className="page-join-us">
 
-        Join us page
+        <section className="hero" style={{backgroundImage: "url('/images/whatwedo/header/image_1.jpg')"}}>
+          <h1 className="hero__title">{get(pageData, 'page_builder.0.attr.heading.value')}</h1>
+          <img className="hero__image" src="/images/whatwedo/header/image_1.jpg" />
+          <DownChevron customClass="hero__down-chevron" ref="downChevron" onClick={this.onClickDownChevron} />
+        </section>
+
+
 
       </article>
     );
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.refs.downChevron.resetAnim();
+      this.refs.downChevron.anim();
+    }, 500);
+  }
+  onClickDownChevron() {
+    ga('send', {
+      'hitType': 'event',
+      'eventCategory': 'hub_page',
+      'eventAction': 'click_animated_chevron',
+      'eventLabel': 'what-we-do'
+    });
   }
 }
