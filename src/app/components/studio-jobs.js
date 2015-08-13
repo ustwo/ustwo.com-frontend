76aadd8a6245a2ffbe23c91cf292cc571bb57997
Phil Linnell <phil@ustwo.com>
2015-08-13 16:48:48 +0100

    jobs components

---
+++ src/app/components/studio-jobs.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import React from 'react';
+import classnames from 'classnames';
+import get from 'lodash/object/get';
+import Flux from '../flux';
+import JobItem from '../components/job-item';
+
+export default class StudioJobs extends React.Component {
+  render() {
+    const classes = classnames('studio-jobs', this.props.studio);
+    return (
+      <div className={classes}>
+        <h3>{this.props.studio}</h3>
+        <div className="studio-info">
+          <p className="excerpt">Join our flagship studio in Shoreditch</p>
+          <p className="content">Occuping three floors of The Tea Building in Shoreditch, our UK studio has over 100 talented and driven people. We're dedicatoed to delivering pixel perfect work for clients including Barclays and Cahnnel4, as well as our own products including Whale Trail and Monument Valley.</p>
+          <div className="photo" style={{backgroundImage: "url(/images/photo.jpg)"}}>
+            <img src="/images/photo.jpg" alt="Tea Building" />
+          </div>
+        </div>
+        <ul className="jobs-list">
+          <JobItem />
+          <JobItem />
+          <JobItem />
+        </ul>
+        <div className="jobs-none">
+          <p>We don't have any openings currently. However we're always looking for talented individuals to join the ustwo family.</p>
+          <a href="#">Get in touch</a>
+        </div>
+      </div>
+    );
+  }
+}
