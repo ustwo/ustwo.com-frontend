152c539cdb311249bdd423b0cee80621f04f01c9
Phil Linnell <phil@ustwo.com>
2015-08-18 11:37:41 +0100

    search page style (needs js)

--- src/app/templates/page-search.js
+++ src/app/templates/page-search.js
@@ -5,17 +5,28 @@ import React from 'react';
 import Flux from '../flux';
 
 export default class PageSearch extends React.Component {
+  componentDidMount() {
+    const input = React.findDOMNode(this.refs.input);
+    input.addEventListener('keyup', this.onKeyup);
+  }
   render() {
+    const searchIcon = '<use xlink:href="/images/spritemap.svg#search" />';
     return (
       <article className="page-search">
-        <form method='POST' action='/blog/search' onSubmit={this.onSubmit}>
-          <input name='q' type='text' placeholder='Search' onChange={this.onChange} />
+        <form method='POST' action='/blog/search' className='search' onSubmit={this.onSubmit}>
+          <input name='q' type='text' className='input' value={this.props.searchQuery} />
+          <div ref='input' contentEditable="true" className='editable-div'></div>
+          <div className="placeholder">Search</div>
+          <button className="submit">
+            <svg role="img" dangerouslySetInnerHTML={{__html: searchIcon }} />
+          </button>
         </form>
       </article>
     );
   }
-  onChange(event) {
-    Flux.setSearchQueryTo(event.target.value);
+  onKeyup = (event) => {
+    event.persist();
+    Flux.setSearchQueryTo(event.target.innerText);
   }
   onSubmit = (event) => {
     event.preventDefault();
