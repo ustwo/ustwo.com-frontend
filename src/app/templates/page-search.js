4c8e9d4c16698497e4d4694ccac0fa67e721088a
Phil Linnell <phil@ustwo.com>
2015-08-18 13:59:35 +0100

    refactored search layout

--- src/app/templates/page-search.js
+++ src/app/templates/page-search.js
@@ -23,16 +23,12 @@ export default class PageSearch extends React.Component {
         <form method='POST' action='/blog/search' className='search' onSubmit={this.onSubmit}>
           <input name='q' type='text' className='input' value={this.props.searchQuery} />
           <div ref='input' contentEditable="true" className='editable-div'></div>
-          <div className="placeholder" onClick={this.onClickPlaceholder}>Search</div>
           <button className="submit">
             <svg role="img" dangerouslySetInnerHTML={{__html: searchIcon }} />
           </button>
         </form>
       </article>
     );
-  }
-  onClickPlaceholder = () => {
-    React.findDOMNode(this.refs.input).focus();
   }
   onKeydown = (event) => {
     if (event.keyCode === 13) {
