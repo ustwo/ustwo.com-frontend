02af175213ba34e5b24394dd8372065dde7490cf
Ethel Ng <ethel@ustwo.com>
2015-08-18 12:30:01 +0100

    Autofocus fake input, handle text entry and submit

--- src/app/templates/page-search.js
+++ src/app/templates/page-search.js
@@ -7,7 +7,14 @@ import Flux from '../flux';
 export default class PageSearch extends React.Component {
   componentDidMount() {
     const input = React.findDOMNode(this.refs.input);
+    input.focus();
     input.addEventListener('keyup', this.onKeyup);
+    input.addEventListener('keydown', this.onKeydown);
+  }
+  componentWillUnmount() {
+    const input = React.findDOMNode(this.refs.input);
+    input.removeEventListener('keyup', this.onKeyup);
+    input.removeEventListener('keydown', this.onKeydown);
   }
   render() {
     const searchIcon = '<use xlink:href="/images/spritemap.svg#search" />';
@@ -16,7 +23,7 @@ export default class PageSearch extends React.Component {
         <form method='POST' action='/blog/search' className='search' onSubmit={this.onSubmit}>
           <input name='q' type='text' className='input' value={this.props.searchQuery} />
           <div ref='input' contentEditable="true" className='editable-div'></div>
-          <div className="placeholder">Search</div>
+          <div className="placeholder" onClick={this.onClickPlaceholder}>Search</div>
           <button className="submit">
             <svg role="img" dangerouslySetInnerHTML={{__html: searchIcon }} />
           </button>
@@ -24,9 +31,16 @@ export default class PageSearch extends React.Component {
       </article>
     );
   }
+  onClickPlaceholder = () => {
+    React.findDOMNode(this.refs.input).focus();
+  }
+  onKeydown = (event) => {
+    if (event.keyCode === 13) {
+      this.onSubmit(event);
+    }
+  }
   onKeyup = (event) => {
-    event.persist();
-    Flux.setSearchQueryTo(event.target.innerText);
+    Flux.setSearchQueryTo(event.target.innerHTML);
   }
   onSubmit = (event) => {
     event.preventDefault();
