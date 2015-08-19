a2c6c87108896a2eb9df31657047e171f1d87b5e
Arnau Siches <arnau@ustwo.com>
2015-08-19 09:55:13 +0100

    Remove static files from node in development

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,6 @@ import cons from 'consolidate';
 import express from 'express';
 import http from 'http';
 import path from 'path';
-import favicon from 'serve-favicon';
 import camelCase from 'lodash/string/camelCase';
 import capitalize from 'lodash/string/capitalize';
 
@@ -12,7 +11,6 @@ import manifest from '../../package.json';
 import routes from './routes.js';
 
 let app = express();
-let publicPath = path.join(__dirname, '../../public');
 
 app.set('port', process.env.PORT || 8888);
 app.set('host', process.env.VIRTUAL_HOST || ('http://localhost:' + app.get('port') + '/'));
@@ -21,8 +19,6 @@ app.engine('html', cons.lodash);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, '../templates'));
 
-app.use(favicon(publicPath + '/favicon.ico'));
-app.use(express.static(publicPath));
 app.use('/', routes);
 
 http.createServer(app).listen(app.get('port'));
