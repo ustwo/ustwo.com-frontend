672912c91fa32c325d9ada6c1b379b447d9cf5f7
Ethel Ng <ethel@ustwo.com>
2015-08-20 18:42:56 +0100

    Add actual sharing component and refactor CSS

--- src/app/templates/page-post.js
+++ src/app/templates/page-post.js
@@ -6,7 +6,7 @@ import classnames from 'classnames';
 import Flux from '../flux';
 
 import ModuleRenderer from '../_lib/module-renderer';
-import SocialMediaStatistics from '../components/social-media-statistics';
+import SocialMediaSharing from '../components/social-media-sharing';
 
 export default class PagePost extends React.Component {
   componentWillMount() {
@@ -17,9 +17,9 @@ export default class PagePost extends React.Component {
   }
   render() {
     const post = this.props.page;
-    const terms = get(post, '_embedded.wp:terms', []);
+    const terms = get(post, '_embedded.wp:term', []);
     const category = get(terms, '0.0');
-    const classes = classnames('page-post', `blog-label-${get(category, 'slug', 'category')}`);
+    const classes = classnames('page-post', `blog-label-${get(category, 'slug', 'uncategorised')}`);
     const attachments = get(post, '_embedded.wp:attachment', []);
 
     return (
@@ -35,12 +35,12 @@ export default class PagePost extends React.Component {
         <div className="content-container">
           <div className="blog-category">{get(category, 'name', 'category')}</div>
           <h1 className="title">{get(post, 'title.rendered')}</h1>
-          <SocialMediaStatistics className='side' facebookShareCount={post && post.facebookShares} twitterShareCount={post && post.twitterShares} />
+          <SocialMediaSharing className='side' title={get(post, 'title.rendered')} uri={`http://ustwo.com/blog/${post.slug}`} facebookShareCount={post && post.facebookShares} twitterShareCount={post && post.twitterShares} />
           <p className="meta">By {get(post, '_embedded.author.0.name')} - <span className="date">{moment(get(post, 'date')).format('D MMMM YYYY')}</span></p>
           <hr className="rule" />
           {get(post, 'page_builder', []).map(this.getModuleRenderer(get(post, 'colors', {})))}
           <hr className="rule" />
-          <SocialMediaStatistics className='bottom' facebookShareCount={post && post.facebookShares} twitterShareCount={post && post.twitterShares} />
+          <SocialMediaSharing className='bottom' title={get(post, 'title.rendered')} uri={`http://ustwo.com/blog/${post.slug}`} facebookShareCount={post && post.facebookShares} twitterShareCount={post && post.twitterShares} />
           <section className="author">
             <img className="mugshot" src={get(post, '_embedded.author.0.avatar_urls.96')} />
             <h1 className="title">About {get(post, '_embedded.author.0.name')}</h1>
