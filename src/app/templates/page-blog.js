21bcfdaaf5243d25592d8573fde2030657e3004d
Ethel Ng <ethel@ustwo.com>
2015-09-02 19:11:47 +0100

    Remove irrelevant initial load and set this.isCategorised on mount

--- src/app/templates/page-blog.js
+++ src/app/templates/page-blog.js
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import classnames from 'classnames';
-import TransitionManager from 'react-transition-manager';
 import find from 'lodash/collection/find';
 import get from 'lodash/object/get';
 import filter from 'lodash/collection/filter';
@@ -11,7 +10,6 @@ import every from 'lodash/collection/every';
 
 import Flux from '../flux';
 
-import BlogLoader from '../components/blog-loader';
 import Hero from '../components/hero';
 import BlogPostListItem from '../components/blog-post-list-item';
 import BlogControls from '../components/blog-controls';
@@ -21,19 +19,13 @@ export default class PageBlog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loadingInitialPosts: true,
       loadingMorePosts: false
     }
   }
   componentWillMount() {
+    this.isCategorised = this.props.blogCategory !== 'all';
     if (this.props.posts) {
       Flux.getSocialSharesForPosts();
-      // TODO: REMOVE THIS
-      setTimeout(() => {
-        this.setState({
-          loadingInitialPosts: false
-        });
-      }, 4000);
     }
   }
   componentWillReceiveProps(nextProps) {
@@ -44,13 +36,9 @@ export default class PageBlog extends React.Component {
     const thereWillBePosts = nextPosts && !!nextPosts.length;
     const newPostsAdded = (currentPosts && nextPosts) && (currentPosts.length < nextPosts.length);
 
-    // applies on cold loading and when category is changed
+    // applies when category is changed
     if (thereAreNoPosts && thereWillBePosts) {
       Flux.getSocialSharesForPosts();
-      // TODO: REMOVE THIS
-      // this.setState({
-      //   loadingInitialPosts: false
-      // });
     }
 
     // applies when "load more" button is clicked
@@ -64,51 +52,28 @@ export default class PageBlog extends React.Component {
   render() {
     const state = this.state;
     const props = this.props;
+    const attachments = get(props.page, '_embedded.wp:attachment.0', []);
+    const image = find(attachments, item => item.id === get(props.page, 'featured_image'));
     let posts = props.posts;
     if (!this.isCategorised && (props.postsPagination < props.postsPaginationTotal)) {
       posts = props.posts && take(props.posts, props.posts.length-2);
     }
     const classes = classnames('page-blog', {
       categorised: this.isCategorised,
-      loading: state.loadingInitialPosts,
+      loading: !posts,
       empty: posts && !posts.length
     });
     return (
       <article className={classes}>
-        <TransitionManager component="div" duration="0">
-          {this.renderBlogContent()}
-        </TransitionManager>
+        <Hero title='Think. Share. Learn.' backgroundTint={true} imageURL={get(image, 'source_url', '')} eventLabel='blog' showDownChevron={false}>
+          <BlogControls blogCategory={props.blogCategory}/>
+        </Hero>
+        <section className="blog-post-list">
+          {this.renderPosts(posts)}
+          <LoadMoreButton loading={state.loadingMorePosts} onClick={this.onClickLoadMore} disabled={props.postsPagination >= props.postsPaginationTotal} />
+        </section>
       </article>
     );
-  }
-  renderBlogContent = () => {
-    const state = this.state;
-    const props = this.props;
-    let content;
-
-    if (state.loadingInitialPosts) {
-      content = <BlogLoader />;
-    } else {
-      const attachments = get(props.page, '_embedded.wp:attachment.0', []);
-      const image = find(attachments, item => item.id === get(props.page, 'featured_image'));
-      let posts = props.posts;
-      if (!this.isCategorised && (props.postsPagination < props.postsPaginationTotal)) {
-        posts = props.posts && take(props.posts, props.posts.length-2);
-      }
-      console.log('image');
-      content = (
-        <div>
-          <Hero title='Think. Share. Learn.' backgroundTint={true} imageURL={get(image, 'source_url', '')} eventLabel='blog' showDownChevron={false}>
-            <BlogControls blogCategory={props.blogCategory}/>
-          </Hero>
-          <section className="blog-post-list">
-            {this.renderPosts(posts)}
-            <LoadMoreButton loading={state.loadingMorePosts} onClick={this.onClickLoadMore} disabled={props.postsPagination >= props.postsPaginationTotal} />
-          </section>
-        </div>
-      );
-    }
-    return content;
   }
   renderPosts = (posts) => {
     let output;
