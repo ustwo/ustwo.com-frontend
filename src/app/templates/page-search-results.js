c0f93e66d9f136236769ab021f72a7d581f7d013
Ethel Ng <ethel@ustwo.com>
2015-09-07 15:27:10 +0100

    Add loading icon to search results page

--- src/app/templates/page-search-results.js
+++ src/app/templates/page-search-results.js
@@ -4,14 +4,36 @@ import React from 'react';
 
 import Flux from '../flux';
 
+import LoadingIcon from '../elements/loading-icon';
 import SearchResultListItem from '../components/search-result-list-item';
 
 export default class PageSearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+  componentWillMount() {
+    if (this.props.posts) {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (this.state.loading && nextProps.posts) {
+      this.setState({
+        loading: false
+      });
+    }
+  }
   render() {
+    const state = this.state;
     const props = this.props;
     let outcomeText;
 
-    if (props.posts && !props.posts.length) {
+    if (!state.loading && !props.posts.length) {
       outcomeText = `No results found for ${props.searchQuery}`;
     } else {
       outcomeText = props.searchQuery;
@@ -23,14 +45,25 @@ export default class PageSearchResults extends React.Component {
           <h1>{outcomeText}</h1>
           <button className='clear-search' onClick={this.onClickClearSearch}>Clear search</button>
         </div>
-        <ul>{this.renderSearchResults()}</ul>
+        {this.renderSearchResults()}
       </article>
     );
   }
   renderSearchResults = () => {
-    return this.props.posts && this.props.posts.map(post => {
-      return <SearchResultListItem data={post} />;
-    });
+    const state = this.state;
+    const props = this.props;
+    let output;
+
+    if (state.loading) {
+      output = <LoadingIcon />;
+    } else {
+      output = (
+        <ul>
+          {props.posts.map(post => <SearchResultListItem data={post} />)}
+        </ul>
+      );
+    }
+    return output;
   }
   onClickClearSearch = () => {
     if (history) {
