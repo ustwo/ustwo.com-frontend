62c68ea32c34c46fcd929461c4a141edcf1686d9
Phil Linnell <phil@ustwo.com>
2015-09-11 16:28:29 +0100

    New, simpler loading spinner

--- src/app/elements/loading-icon.js
+++ src/app/elements/loading-icon.js
@@ -6,15 +6,12 @@ export default class LoadingIcon extends React.Component {
   render() {
     return (
       <div className={classnames("loading-icon", this.props.className)}>
-        <div className="wrapper">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        <div className="opposite">
-          <div></div>
-        </div>
+				<span className="half left">
+          <span className="bg"></span>
+        </span>
+				<span className="half right">
+          <span className="bg"></span>
+        </span>
       </div>
     );
   }
