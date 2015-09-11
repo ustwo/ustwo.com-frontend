6c4fc13bc0322e18ad818317b833cb78b7b8cc3a
Nick Collings <nick@collingo.com>
2015-09-11 15:13:06 +0100

    Running simple test suites

--- tasks/compiler.mk
+++ tasks/compiler.mk
@@ -11,6 +11,7 @@ define compile
 		-v $(BASE_PATH)/compiler.json:/usr/local/src/package.json \
 		-v $(BASE_PATH)/share/nginx/assets:/usr/local/src/public \
 		-v $(BASE_PATH)/src:/usr/local/src/src \
+		-v $(BASE_PATH)/test:/usr/local/src/test \
 		$(compiler_image) $1
 endef
 
