fb2b933eb955a2ce82a654410532c4d8d55f6506
Arnau Siches <arnau@ustwo.com>
2015-09-12 15:39:53 +0100

    Fix compiler volumes

--- tasks/compiler.mk
+++ tasks/compiler.mk
@@ -11,7 +11,8 @@ compiler_volumes = \
   -v $(BASE_PATH)/gulpfile.js:/usr/local/src/gulpfile.js \
   -v $(BASE_PATH)/compiler.json:/usr/local/src/package.json \
   -v $(BASE_PATH)/share/nginx/assets:/usr/local/src/public \
-  -v $(BASE_PATH)/src:/usr/local/src/src \
+  -v $(BASE_PATH)/test:/usr/local/src/test \
+  -v $(BASE_PATH)/src:/usr/local/src/src
 
 define compile
 	$(if $(CI), $(DOCKER_CI_TASK), $(DOCKER_TASK)) \
