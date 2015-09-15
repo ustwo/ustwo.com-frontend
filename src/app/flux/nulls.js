34e93f3c63a648854bcbdd75b9504e0e92762ead
Ethel Ng <ethel@ustwo.com>
2015-09-15 18:36:29 +0100

    Remove unused items from Store/nulls

--- src/app/flux/nulls.js
+++ src/app/flux/nulls.js
@@ -6,42 +6,5 @@ export default {
   twitterShares: null,
   facebookShares: null,
   postsPaginationTotal: null,
-  posts: null,
-  caseStudy: {
-    "name": null,
-    "link": null,
-    "excerpt": null,
-    "bgColor": {
-      "name": "bgColor",
-      "value": null,
-      "type": "color"
-    },
-    "primaryColor": {
-      "name": "primaryColor",
-      "value": null,
-      "type": "color"
-    },
-    "secondaryColor": {
-      "name": "secondaryColor",
-      "value": null,
-      "type": "color"
-    },
-    "page_builder": [],
-    "featured_image": null,
-    "_links": {}
-  },
-  attachment: {
-    "_links": {},
-    "alt_text": null,
-    "author": null,
-    "date": null,
-    "id": null,
-    "link": null,
-    "media_details": {},
-    "media_type": null,
-    "slug": null,
-    "source_url": null,
-    "title": {},
-    "type": "attachment"
-  }
+  posts: null
 };
