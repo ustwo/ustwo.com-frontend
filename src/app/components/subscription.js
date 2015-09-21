92b2dbdc21fae9d6992b49c03979e2c3d4320471
Ethel Ng <ethel@ustwo.com>
2015-09-21 15:23:36 +0100

    Add Mailchimp integration to Subscription

--- src/app/components/subscription.js
+++ src/app/components/subscription.js
@@ -9,9 +9,12 @@ export default class Subscription extends React.Component {
       <div className="subscription">
         <h2>ustwo News</h2>
         <p>Give us your email and we'll keep you in the loop with our latest projects and thoughts.</p>
-        <form>
+        <form action="//ustwo.us10.list-manage.com/subscribe/post?u=7f1269c0305abed7c91a24b97&amp;id=e6835a8563" method="POST" target="_blank">
           <label htmlFor="subscription-email">{labelText}</label>
-          <input name="email" id="subscription-email" placeholder={labelText} />
+          <input name="EMAIL" id="subscription-email" placeholder={labelText} required={true} />
+          <div style={{ position: "absolute", left: -5000 }}>
+            <input type="text" name="b_7f1269c0305abed7c91a24b97_e6835a8563" tabIndex="-1" value="" />
+          </div>
           <button type="submit">Subscribe</button>
         </form>
       </div>
