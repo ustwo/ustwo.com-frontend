4f7907c4258bd24d90bfc10b19ad09c78a26837e
Nick Collings <nick@collingo.com>
2015-09-30 18:01:37 +0100

    Basic sandboxing for grid-cell and job-item components

---
+++ src/app/components/job-item/sandbox.js
@@ -0,0 +1,26 @@
+import JobItem from './';
+
+const job = {
+  key: "5a6e",
+  title: "Agile Team Coach / PM",
+  full_title: "Agile Team Coach / PM - London",
+  code: "",
+  shortcode: "58E3D60B6A",
+  state: "published",
+  department: "",
+  url: "https://ustwo.workable.com/jobs/21916",
+  application_url: "https://ustwo.workable.com/jobs/21916/candidates/new",
+  shortlink: "https://ustwo.workable.com/j/58E3D60B6A",
+  location: {
+    country: "United Kingdom",
+    country_code: "GB",
+    region: "London, City of",
+    region_code: "LND",
+    city: "London",
+    zip_code: "E1 6JJ",
+    telecommuting: ""
+  },
+  created_at: "2014-10-08"
+}
+
+export default (<JobItem job={job} />);
