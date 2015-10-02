8b689db973686449b6959ac571ed067e4d084fb2
Arnau Siches <arnau@ustwo.com>
2015-10-02 14:25:26 +0100

    Tests on uglify

--- src/scripts/spa.sh
+++ src/scripts/spa.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+filename="/usr/local/src/public/js/app.js"
+
 if [[ "$VERBOSE" -eq "true" ]]; then
   browserify_verbose="--debug"
 else
@@ -16,5 +18,21 @@ browserify /usr/local/src/src/app/index.js \
            --external babelify/polyfill \
            --external react \
            --external svg4everybody \
-| $uglify \
-> /usr/local/src/public/js/app.js
+           --external babel \
+           --external classnames \
+           --external ellipsize \
+           --external es6-promise \
+           --external gsap \
+           --external lodash \
+           --external moment \
+           --external react-helmet \
+           --external react-style-guide \
+           --external react-transition-manager \
+           --external scrollmagic \
+           --outfile $filename
+
+echo ">>> $VERBOSE"
+if [[ "$VERBOSE" -ne "true" ]]; then
+  echo "here"
+  uglifyjs --mangle --comments --stats -o $filename -- $filename
+fi
