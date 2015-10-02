36761c77f09c3324015c0eb391a01de853b9a9fd
Arnau Siches <arnau@ustwo.com>
2015-10-02 16:08:30 +0100

    Fix vendors

--- src/scripts/vendors.sh
+++ src/scripts/vendors.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e
 
-browserify -r babelify/polyfill \
-           -r react \
-           -r svg4everybody \
+base="/usr/local/src"
+filename="$base/public/js/vendors.js"
+
+browserify --require babelify/polyfill \
+           --require react \
+           --require svg4everybody \
+           --require babel \
+           --require classnames \
+           --require ellipsize \
+           --require es6-promise \
+           --require gsap \
+           --require lodash \
+           --require moment \
+           --require react-helmet \
+           --require react-style-guide \
+           --require react-transition-manager \
+           --require scrollmagic \
 | uglifyjs --mangle \
            --comments \
            --stats \
-           -o /usr/local/src/public/js/vendors.js
+           -o $filename
