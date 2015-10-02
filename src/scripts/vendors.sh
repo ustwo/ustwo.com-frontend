ec9d883455ab8136b5ad0fa3176a4a88aaaf2bb9
Arnau Siches <arnau@ustwo.com>
2015-10-02 18:30:06 +0100

    Remove unnecessary dependencies

--- src/scripts/vendors.sh
+++ src/scripts/vendors.sh
@@ -7,15 +7,10 @@ filename="$base/public/js/vendors.js"
 browserify --require babelify/polyfill \
            --require react \
            --require svg4everybody \
-           --require babel \
            --require classnames \
            --require ellipsize \
            --require es6-promise \
-           --require gsap \
-           --require lodash \
            --require moment \
-           --require react-helmet \
-           --require react-style-guide \
            --require react-transition-manager \
            --require scrollmagic \
 | uglifyjs --mangle \
