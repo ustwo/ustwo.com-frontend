de03dde2e0480e0b4e0bc3127a2334afbf3a1ef9
Arnau Siches <arnau@ustwo.com>
2015-10-05 11:31:11 +0100

    Remove unused code

--- src/scripts/spa.js
+++ src/scripts/spa.js
@@ -9,30 +9,4 @@ function spa() {
   return exec("./src/scripts/spa.sh");
 }
 
-    // var bundler = browserify({
-    //                 debug: verbose,
-    //                 // insertGlobals: false,
-    //                 // detectGlobals: false,
-    //                 cache: {},
-    //                 packageCache: {}})
-    //               .require(require.resolve('./src/app/index.js'),
-    //                        { entry: true })
-    //               .transform(babelify.configure({
-    //                   optional: ["es7.classProperties"]}))
-    //               .transform(aliasify, require('./package.json').aliasify)
-    //               .external('babelify/polyfill')
-    //               .external('react')
-    //               .external('svg4everybody');
-
-    // return bundler.bundle()
-    //         .on('error', handleError('Browserify'))
-    //         .pipe(source('app.js'))
-    //         .pipe(buffer())
-    //         .pipe(gulpif(!verbose, uglify()))
-    //         .pipe(gulp.dest('public/js'));
-    //         // .pipe(fs.createWriteStream(path.join(__dirname,
-    //         //                                      'public/js/app.js'), 'utf8'));
-  // },
-
-
 export default spa;
