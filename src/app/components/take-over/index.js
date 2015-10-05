28408dcd7c153aacdadddcc16d558d33689ae4ff
Nick Collings <nick@collingo.com>
2015-10-05 16:39:50 +0100

    Setup ugly static takeover

--- src/app/components/take-over/index.js
+++ src/app/components/take-over/index.js
@@ -1,29 +1,105 @@
 import React from 'react';
 import classnames from 'classnames';
 import TransitionManager from 'react-transition-manager';
+import get from 'lodash/object/get';
 
 import Track from '../../adaptors/server/track';
 import Flux from '../../flux';
+import getFeaturedImage from '../../lib/get-featured-image';
 
 import CloseButton from '../close-button';
 import NewsFlash from '../news-flash';
+import Rimage from '../rimage';
 
 import {onClickContent} from '../modal';
 
 const takeover = {
-  title: "Moodnotes - Out Now!",
-  name: "Moodnotes",
-  description: "Capture your feelings and improve your thinking habits",
+  name: "Pause",
+  title: "PAUSE",
+  description: "Relaxation at your fingertip",
+  featured_image: 8672,
   links: [{
     type: 'http',
     text: 'Download on iOS',
-    url: 'http://us2.co/Moodnotes'
+    url: 'http://us2.co/Pause'
   }, {
     type: 'http',
-    text: 'Go to moodnotesapp.com',
-    url: 'http://moodnotes.thriveport.com/'
+    text: 'Go to getpauseapp.com',
+    url: 'http://getpauseapp.com/'
   }],
-  image: 'images/home/moodnotes_takeover_image.png'
+  _embedded: {
+    "wp:attachment": [
+      {
+        "id": 8672,
+        "date": "2015-10-05T10:58:17",
+        "slug": "pause",
+        "type": "attachment",
+        "link": "http://ustwo-staging.aws.hmn.md/blog/takeover/pause/pause/",
+        "title": {
+          "rendered": "pause"
+        },
+        "author": 3,
+        "alt_text": "",
+        "media_type": "image",
+        "source_url": "https://hmn-uploads.s3.amazonaws.com/ustwo-staging/uploads/2015/10/pause.png",
+        "media_details": {
+          "width": 670,
+          "height": 1028,
+          "file": "2015/10/pause.png",
+          "sizes": {
+            "thumbnail": {
+              "file": "pause-300x300.png",
+              "width": 300,
+              "height": 300,
+              "mime-type": "image/png",
+              "source_url": "https://hmn-uploads.s3.amazonaws.com/ustwo-staging/uploads/2015/10/pause-300x300.png"
+            },
+            "medium": {
+              "file": "pause-501x768.png",
+              "width": 501,
+              "height": 768,
+              "mime-type": "image/png",
+              "source_url": "https://hmn-uploads.s3.amazonaws.com/ustwo-staging/uploads/2015/10/pause-501x768.png"
+            },
+            "small": {
+              "file": "pause-313x480.png",
+              "width": 313,
+              "height": 480,
+              "mime-type": "image/png",
+              "source_url": "https://hmn-uploads.s3.amazonaws.com/ustwo-staging/uploads/2015/10/pause-313x480.png"
+            },
+            "small_crop": {
+              "file": "pause-640x480.png",
+              "width": 640,
+              "height": 480,
+              "mime-type": "image/png",
+              "source_url": "https://hmn-uploads.s3.amazonaws.com/ustwo-staging/uploads/2015/10/pause-640x480.png"
+            },
+            "medium_crop": {
+              "file": "pause-670x768.png",
+              "width": 670,
+              "height": 768,
+              "mime-type": "image/png",
+              "source_url": "https://hmn-uploads.s3.amazonaws.com/ustwo-staging/uploads/2015/10/pause-670x768.png"
+            }
+          },
+          "image_meta": {
+              "aperture": 0,
+              "credit": "",
+              "camera": "",
+              "caption": "",
+              "created_timestamp": 0,
+              "copyright": "",
+              "focal_length": 0,
+              "iso": 0,
+              "shutter_speed": 0,
+              "title": "",
+              "orientation": 0
+          }
+        }
+      }
+    ]
+  }
 };
 
 export default class TakeOver extends React.Component {
@@ -35,18 +111,20 @@ export default class TakeOver extends React.Component {
   }
   render = () => {
     let content;
+    const image = getFeaturedImage(takeover);
+    // const takeover = this.props.takeover;
     if(this.state.showContent) {
       content = (
         <div key="detail" className="take-over__content">
           <div className="take-over__content__message">
             <CloseButton onClose={this.onClickClose} className="take-over__content__message__close" autoAnim={1000} />
-            <h1 className="take-over__content__message__title">{takeover.title}</h1>
+            <Rimage wrap="div" className="take-over__content__image"  sizes={get(image, 'media_details.sizes')}>
+            </Rimage>
+            <h1 className="take-over__content__message__title" style={{color: "#2a88a9"}}>{takeover.title}</h1>
             <p className="take-over__content__message__description">{takeover.description}</p>
             <ul className="take-over__content__message__links">
               {takeover.links.map(this.renderLink)}
             </ul>
-          </div>
-          <div className="take-over__content__image" style={{backgroundImage: `url(${takeover.image})`}}>
           </div>
         </div>
       );
@@ -74,7 +152,7 @@ export default class TakeOver extends React.Component {
         prefix = "tel:";
       break;
     }
-    return <li className={`take-over__content__message__links__link-item ${link.type}`}><a className="take-over__content__message__links__link-item__link" target="_blank" href={`${prefix}${link.url}`} onClick={this.onClickLink(index)}>{link.text}</a></li>;
+    return <li className={`take-over__content__message__links__link-item ${link.type}`}><a className="take-over__content__message__links__link-item__link" target="_blank" href={`${prefix}${link.url}`} onClick={this.onClickLink(index)} style={{color: "#2a88a9"}}>{link.text}</a></li>;
   }
   componentDidMount() {
     this.contentTimeout = setTimeout(() => {
@@ -87,6 +165,7 @@ export default class TakeOver extends React.Component {
     clearTimeout(this.contentTimeout);
   }
   onClickClose() {
+    // const takeover = this.props.takeover;
     Track('send', {
       'hitType': 'event',          // Required.
       'eventCategory': 'takeover',   // Required.
@@ -96,6 +175,7 @@ export default class TakeOver extends React.Component {
     Flux.closeTakeover();
   }
   onClickLink(index) {
+    // const takeover = this.props.takeover;
     return (e) => {
       Track('send', {
         'hitType': 'event',          // Required.
--- src/app/flux/store.js
+++ src/app/flux/store.js
@@ -88,7 +88,7 @@ export default {
       // - it doesn't exist in the store OR
       // - it exists in the store with a different slug OR
       // - posts have a different blog category
-      return (!_state[item.type] || (_state[item.type].slug && _state[item.type].slug !== item.slug) || (item.slug && item.slug.match(/posts\/\w+/) && item.slug.split('/')[1] !== _state.blogCategory));
+      return (!_state[item.type] || (item.slug && _state[item.type].slug && _state[item.type].slug !== item.slug) || (item.slug && item.slug.match(/posts\/\w+/) && item.slug.split('/')[1] !== _state.blogCategory));
     });
     return DataLoader(itemsToLoad, applyData).then(() => _state);
   },
