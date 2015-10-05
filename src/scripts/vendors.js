63024cacd8357cba20f433172752a45e421dc3ba
Arnau Siches <arnau@ustwo.com>
2015-10-05 12:02:01 +0100

    Fix external aliases

--- src/scripts/vendors.js
+++ src/scripts/vendors.js
@@ -1,4 +1,14 @@
 import {exec} from 'child_process';
+// import browserify from 'browserify';
+// import minifyify from 'minifyify';
+// import fs from 'fs';
+// import path from 'path';
+// import uglify from 'uglify-js';
+// import through from 'through2';
+// import buffer from 'vinyl-buffer';
+// import source from 'vinyl-source-stream';
+
+// import {handleError} from './helpers';
 
 /*
  * Creates a Vendor bundle
@@ -8,5 +18,49 @@ import {exec} from 'child_process';
 function vendors() {
   return exec("./src/scripts/vendors.sh");
 }
+
+
+// function vendors() {
+//   const vendorPath = path.join(__dirname, '../../public/js/vendors.js');
+//   const vendorMapPath = path.join(__dirname, '../../public/js/vendors.map.json');
+//   // const vendorMapPath = 'vendors.map.json';
+
+//   return browserify({noParse: false})
+//             .require('babelify/polyfill')
+//             .require('react')
+//             .require('svg4everybody')
+//             .on('error', handleError('Browserify'))
+//             .bundle()
+//             // .pipe(source('vendors.js'))
+//             // .pipe(buffer())
+//             // .pipe(minify())
+//             .pipe(fs.createWriteStream(vendorPath), 'utf8');
+// }
+
+// function minify() {
+//   return through.obj(function (chunk, encoding, next) {
+//     const options = {fromString: true, output: {}};
+//     let str = chunk;
+//     // let str = chunk.toString();
+
+//     console.log(str.toString());
+
+//     // let mangled = uglify.minify(str, options);
+
+//     // chunk.source = new Buffer(mangled.code);
+
+//     // console.log(">>", mangled);
+
+//     // this.push(chunk);
+
+//     next();
+//   })
+//   .on('data', function (data) {
+//     // console.log(">>", data.isStream());
+//   })
+//   .on('end', function () {
+//     console.log("end");
+//   })
+// }
 
 export default vendors;
