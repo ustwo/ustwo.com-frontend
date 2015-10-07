d9b8a83086e72d86ffbe28008e9f139bb924d27b
Nick Collings <nick@collingo.com>
2015-10-07 16:10:42 +0100

    ContactDetail sandbox and encapsulation of styles

---
+++ src/app/components/contact-detail/sandbox.js
@@ -0,0 +1,22 @@
+import ContactDetail from './';
+
+const detail = {
+  "title": "Work with us",
+  "desc": "Have a project or an idea you’d like to collaborate with ustwo on? Let’s talk.",
+  "type": "work",
+  "methods": [{
+    "type": "email",
+    "text": "work@ustwo.com",
+    "uri": "mailto:work@ustwo.com"
+  }]
+};
+
+const Sandbox = React.createClass({
+  render() {
+    return (<div style={{height: '100%'}}>
+      <ContactDetail detail={detail} />
+    </div>);
+  }
+});
+
+export default Sandbox;
