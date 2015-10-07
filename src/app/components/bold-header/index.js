7f6276fef13c7f70f6754ffc3eb3f90b010e541a
Nick Collings <nick@collingo.com>
2015-10-07 15:46:17 +0100

    BoldHeader sandbox and classnames tidy

--- src/app/components/bold-header/index.js
+++ src/app/components/bold-header/index.js
@@ -1,17 +1,18 @@
 'use strict';
 
 import React from 'react';
+import classnames from 'classnames';
 
 import BoldHeaderSubtitle from '../bold-header-subtitle';
 
 export default class BoldHeader extends React.Component {
   render() {
-    const titleClass = `u-text-${this.props.colour}`;
+    const titleClass = classnames(this.props.colour && `u-text-${this.props.colour}`);
     let subtitleMarkup = '';
     if (this.props.subtitle) {
       subtitleMarkup = <BoldHeaderSubtitle>{this.props.subtitle}</BoldHeaderSubtitle>
     }
-    const classes = `bold-header ${this.props.customClass}`;
+    const classes = classnames('bold-header', this.props.customClass && `${this.props.customClass}`);
     return (
       <header className={classes}>
         <h1 className={titleClass} ref="title">
