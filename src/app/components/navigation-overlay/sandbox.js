4997b34098d0de08d9e888cb33dba9dee2e835de
Nick Collings <nick@collingo.com>
2015-10-12 15:42:08 +0100

    NavigationOverlay sandbox

---
+++ src/app/components/navigation-overlay/sandbox.js
@@ -0,0 +1,34 @@
+import NavigationOverlay from './';
+
+const pages = [{
+  id: 1,
+  slug: 'home',
+  colour: 'red',
+  ga: 'home',
+  title: 'Home'
+}, {
+  id: 2,
+  slug: 'what-we-do',
+  colour: 'blue',
+  ga: 'what_we_do',
+  title: 'What we do'
+}, {
+  id: 3,
+  slug: 'join-us',
+  colour: 'green',
+  ga: 'join_us',
+  title: 'Join us'
+}];
+
+const Sandbox = React.createClass({
+  render() {
+    return (<div>
+      <NavigationOverlay
+        section="what-we-do"
+        pages={pages}
+      />
+    </div>);
+  }
+});
+
+export default Sandbox;
