2159bc80e0033b823dc0e0f3dfb4bdd106c02e1c
Daniel Demmel <dain@danieldemmel.me>
2015-10-12 10:42:17 +0100

    Make sure we're compiling font.css.

--- src/scripts/css.sh
+++ src/scripts/css.sh
@@ -6,6 +6,8 @@ echo "Compiling CSS..."
 base="/home/ustwo"
 input="$base/src/app/index.scss"
 filename="$base/public/css/index.css"
+fontinput="$base/src/app/lib/font.scss"
+fontfilename="$base/public/css/font.css"
 
 mkdir -p $base/public/css
 
@@ -25,5 +27,9 @@ postcss --use autoprefixer \
         --autoprefixer.browser "last 2 versions" \
         --output $filename \
         $filename
+
+sassc $fontinput \
+      --style compressed \
+      $fontfilename
 
 echo "Done with CSS"
