a549864f15cb9bceb56b02fbfb92eb49d1850ddc
Arnau Siches <arnau@ustwo.com>
2015-10-13 14:10:52 +0100

    Clean image process

--- src/scripts/images.sh
+++ src/scripts/images.sh
@@ -9,11 +9,9 @@ if [[ "$VERBOSE" == "true" ]]; then
   rsync_verbose="-v"
 fi
 
-mkdir -p $base/public/images
-
 rsync -r \
       $rsync_verbose \
-      $base/src/app/images/ \
-      $base/public/images/
+      $base/src/app/images \
+      $base/public
 
 echo "Done processing images..."
