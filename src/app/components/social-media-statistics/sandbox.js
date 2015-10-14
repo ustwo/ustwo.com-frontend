a498115cda15c2c3ae9a46306ebe6039fea78ec2
Nick Collings <nick@collingo.com>
2015-10-14 14:31:22 +0100

    SocialMediaStatistics sandbox

---
+++ src/app/components/social-media-statistics/sandbox.js
@@ -0,0 +1,14 @@
+import SocialMediaStatistics from './';
+
+const Sandbox = React.createClass({
+  render() {
+    return (<div className="sandbox">
+      <SocialMediaStatistics
+        facebookShares={34}
+        twitterShares={15}
+      />
+    </div>);
+  }
+});
+
+export default Sandbox;
