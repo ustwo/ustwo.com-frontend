14d390576352849f3090c4cf0f0de8671b3d1d7e
Nick Collings <nick@collingo.com>
2015-10-14 12:19:00 +0100

    Search sandbox

---
+++ src/app/components/search/sandbox.js
@@ -0,0 +1,14 @@
+import Search from './';
+
+const Sandbox = React.createClass({
+  render() {
+    return (<div className="sandbox">
+      <Search
+        className="test-class"
+        searchQuery="design"
+      />
+    </div>);
+  }
+});
+
+export default Sandbox;
