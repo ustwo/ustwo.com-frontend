acb7fa96e51946cd5033279f6e76aaeaa0cdbf7a
Nick Collings <nick@collingo.com>
2015-10-14 15:40:13 +0100

    Takevover sandbox

---
+++ src/app/components/take-over/sandbox.js
@@ -0,0 +1,11 @@
+import Takeover from './';
+
+const Sandbox = React.createClass({
+  render() {
+    return (<div className="sandbox">
+      <Takeover />
+    </div>);
+  }
+});
+
+export default Sandbox;
