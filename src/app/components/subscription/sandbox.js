225119056819fca0368194597d758aecd26f3ae1
Nick Collings <nick@collingo.com>
2015-10-14 15:19:26 +0100

    Subscription sandbox

---
+++ src/app/components/subscription/sandbox.js
@@ -0,0 +1,11 @@
+import Subscription from './';
+
+const Sandbox = React.createClass({
+  render() {
+    return (<div className="sandbox">
+      <Subscription />
+    </div>);
+  }
+});
+
+export default Sandbox;
