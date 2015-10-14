81077bb69a04e337f9c42d607c814e79a90f6c33
Nick Collings <nick@collingo.com>
2015-10-14 15:34:29 +0100

    SVG sandbox

---
+++ src/app/components/svg/sandbox.js
@@ -0,0 +1,16 @@
+import SVG from './';
+
+const Sandbox = React.createClass({
+  render() {
+    return (<div className="sandbox">
+      <SVG
+        className="test-class"
+        title="test-title"
+        spritemapID="ustwologo"
+        style={{fill: 'red'}}
+      />
+    </div>);
+  }
+});
+
+export default Sandbox;
