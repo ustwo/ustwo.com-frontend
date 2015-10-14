3eab3d746858dfc7d5910e8c516cd3f3cfb3de21
Nick Collings <nick@collingo.com>
2015-10-14 15:51:24 +0100

    WordAnimation sandbox

---
+++ src/app/components/word-animation/sandbox.js
@@ -0,0 +1,16 @@
+import WordAnimation from './';
+
+const Sandbox = React.createClass({
+  render() {
+    return (<div className="sandbox">
+      <WordAnimation
+        delay={1}
+        duration={3}
+      >
+        Some words to be animated in
+      </WordAnimation>
+    </div>);
+  }
+});
+
+export default Sandbox;
