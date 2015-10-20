eeed1d043f8f0e426189075723588f25f57a6fc1
Ethel Ng <ethel@ustwo.com>
2015-10-20 17:14:08 +0100

    Fix 404 sandbox

--- src/app/components/404/sandbox.js
+++ src/app/components/404/sandbox.js
@@ -3,6 +3,11 @@ import FourOhFour from './';
 const Sandbox = React.createClass({
   render() {
     return <div className="sandbox">
+      <style>{`
+        .sandbox {
+          height: 100vh;
+        }
+      `}</style>
       <FourOhFour />
     </div>;
   }
