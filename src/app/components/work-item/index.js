bb2d07c5fff64cce1b3dcd9cb3891ddb228198f7
Phil Linnell <phil@ustwo.com>
2015-10-22 17:54:55 +0100

    Bye BEM - work-item

--- src/app/components/work-item/index.js
+++ src/app/components/work-item/index.js
@@ -16,12 +16,12 @@ export default class WorkItem extends React.Component {
     const image = getFeaturedImage(data, attachments);
     return (
       <li className={classnames(className, 'work-item')} style={{backgroundColor: get(data, 'colors.bg')}}>
-        <Rimage wrap="div" href={link} onClick={Flux.override(link)} className="work-item__image-link" sizes={get(image, 'media_details.sizes')} />
-        <div className="work-item__details">
-          <p className="work-item__details__type" style={{borderColor: get(data, 'colors.secondary')}}>{get(data, 'type')}</p>
-          <h3 className="work-item__details__title" style={{color: get(data, 'colors.primary')}}><a href={link} onClick={Flux.override(link)}>{get(data, 'name')}</a></h3>
-          <div className="work-item__details__desc" dangerouslySetInnerHTML={{__html: get(data, 'excerpt')}} />
-          <a className="work-item__details__cta" href={link} style={{borderColor: get(data, 'colors.secondary')}} onClick={Flux.override(link)}>Read more</a>
+        <Rimage wrap="div" href={link} onClick={Flux.override(link)} className="image" sizes={get(image, 'media_details.sizes')} />
+        <div className="details">
+          <p className="type" style={{borderColor: get(data, 'colors.secondary')}}>{get(data, 'type')}</p>
+          <h3 className="title" style={{color: get(data, 'colors.primary')}}><a href={link} onClick={Flux.override(link)}>{get(data, 'name')}</a></h3>
+          <div className="description" dangerouslySetInnerHTML={{__html: get(data, 'excerpt')}} />
+          <a className="link" href={link} style={{borderColor: get(data, 'colors.secondary')}} onClick={Flux.override(link)}>Read more</a>
         </div>
       </li>
     );
