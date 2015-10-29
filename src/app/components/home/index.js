36fae9576a01f81c4a1ae4c6f55e100b99710f2f
Phil Linnell <phil@ustwo.com>
2015-10-29 15:18:50 +0000

    now using hero css styles

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -148,12 +148,12 @@ const PageHome = React.createClass({
     // End Chevron
     return (
       <article className="page-home">
-        <ScreenBlock ref="blockWelcome" customClass="welcome" hexColour={'#' + this.state.blocks[0].hexColour}>
+        <ScreenBlock ref="blockWelcome" customClass="welcome hero" hexColour={'#' + this.state.blocks[0].hexColour}>
           <EntranceTransition className="image-entrance">
             <div className="headline-image"></div>
           </EntranceTransition>
           <EntranceTransition className="title-entrance">
-            <div className="headline-text">
+            <div className="headline-text title">
               <BoldHeader colour="white">
                 <WordAnimation delay={1} duration={0.4}>
                   We're a digital product studio
