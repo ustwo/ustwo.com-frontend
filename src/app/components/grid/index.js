987df4d8e32584293062d8b365ee94dc0eb9904a
Phil Linnell <phil@ustwo.com>
2015-11-05 17:15:33 +0000

    changes to grid-cell layouts

--- src/app/components/grid/index.js
+++ src/app/components/grid/index.js
@@ -4,36 +4,12 @@ import kebabCase from 'lodash/string/kebabCase';
 import get from 'lodash/object/get';
 import QS from '../../lib/query-string';
 
-import Rimage from '../rimage';
 import GridCell from '../grid-cell';
 
 const Grid = React.createClass({
-  renderVideo() {
-    const { video } = this.props;
-    let output;
-    if (video && video.length) {
-      const baseURL = 'https://player.vimeo.com/video';
-      const options = {
-        title: 0,
-        byline: 0,
-        portrait: 0
-      }
-      output = <iframe
-        src={`${baseURL}/${video}${QS.stringify(options)}`}
-        frameBorder="0"
-        webkitallowfullscreen
-        mozallowfullscreen
-        allowFullScreen
-      />;
-    }
-    return output;
-  },
   render() {
     const { className, images, cells } = this.props;
     return <section className={classnames('grid', className)}>
-      <Rimage className="video" wrap="div" sizes={images}>
-        {this.renderVideo()}
-      </Rimage>
       <ul className="grid-list">
         {cells.map(cell => {
           return <GridCell
