6e5628a17184afc8b3b101686a056054f6d80ae4
Phil Linnell <phil@ustwo.com>
2015-11-09 13:34:06 +0000

    Created related-content component

---
+++ src/app/components/related-content/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+export default class RelatedContent extends React.Component {
+  render() {
+    return (
+      <div className="related-content">
+        <h2>More Juice</h2>
+      </div>
+    );
+  }
+}
---
+++ src/app/components/related-content/sandbox.js
@@ -0,0 +1,11 @@
+import RelatedContent from './';
+
+const Sandbox = React.createClass({
+  render() {
+    return <div className="sandbox full-width-component">
+      <RelatedContent />
+    </div>;
+  }
+});
+
+export default Sandbox;
