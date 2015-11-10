dff95e9f327c6e0f064994fe3b15220f2020e5cf
Ethel Ng <ethel@ustwo.com>
2015-11-10 10:33:59 +0000

    Tidy up markup for QuoteBlock

--- src/app/components/quote-block/index.js
+++ src/app/components/quote-block/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 import SVG from '../svg';
 
-export default class QuoteBlock extends React.Component {
+class QuoteBlock extends React.Component {
   render() {
-    return (
-      <div className="quote-block" style={{ backgroundColor: this.props.backgroundColour }}>
-        <div className="quote-mark">
-          <SVG role="presentation" spritemapID='quotemark' />
-        </div>
-        <div className="quote" dangerouslySetInnerHTML={{ __html: this.props.children }}></div>
-        <p className="source">{this.props.source}</p>
+    const { backgroundColour, children, source } = this.props;
+    return <div
+      className='quote-block'
+      style={{ backgroundColor: backgroundColour }}
+    >
+      <div className='quote-mark'>
+        <SVG role='presentation' spritemapID='quotemark' />
       </div>
-    );
+      <div
+        className='quote'
+        dangerouslySetInnerHTML={{ __html: children }}
+      />
+      <p className='source'>By {source}</p>
+    </div>;
   }
 }
+
+export default QuoteBlock;
