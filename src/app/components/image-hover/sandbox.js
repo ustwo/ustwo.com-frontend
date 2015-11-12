64a5ed276b298ca7d819bedafbf0a3b60c24dc8a
Phil Linnell <phil@ustwo.com>
2015-11-12 12:34:47 +0000

    More rollovers and transitions for them + finished up image-hover

--- src/app/components/image-hover/sandbox.js
+++ src/app/components/image-hover/sandbox.js
@@ -4,15 +4,8 @@ import renderVariations from '../../lib/sandbox/render-variations';
 const Sandbox = React.createClass({
   render() {
     return <div className="sandbox">
-      <style>{`
-        body {
-          background: #666;
-        }
-      `}</style>
       {renderVariations({
-        '500ms timeout': <ImageHover autoAnim={500} />,
-      '1.5s timeout': <ImageHover autoAnim={1500} />,
-    'With onClick handler': <ImageHover autoAnim={500} onClick={() => alert('clicked')} />
+        '500ms timeout': <ImageHover autoAnim={500} />
       })}
     </div>;
   }
