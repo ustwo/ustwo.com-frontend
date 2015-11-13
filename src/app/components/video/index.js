114a8185bda2736d6779af7edc598c1b181fe323
Ethel Ng <ethel@ustwo.com>
2015-11-13 12:09:19 +0000

    Play video automatically on loop

--- src/app/components/video/index.js
+++ src/app/components/video/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Video = React.createClass({
   render() {
+    const videoURL = "https://player.vimeo.com/external/143640008.hd.mp4?s=7c2a73ce981cae52bedc594546b6d27e17ca4f7b&profile_id=113";
+    const posterURL = "/images/transparent.png";
+
     return <div className="video">
+      <video src={videoURL} poster={posterURL} autoPlay loop />
     </div>;
   }
 });
--- src/app/components/video/sandbox.js
+++ src/app/components/video/sandbox.js
@@ -3,6 +3,11 @@ import Video from './';
 const Sandbox = React.createClass({
   render() {
     return <div className="sandbox full-width-component">
+      <style>{`
+        .sandbox {
+          height: 100vh;
+        }
+      `}</style>
       <Video />
     </div>;
   }
