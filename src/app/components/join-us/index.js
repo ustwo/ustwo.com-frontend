f5c60bf83fff90547a04a088222bd850ce70ffe3
Nick Collings <nick@collingo.com>
2015-11-23 12:39:11 +0000

    Simplify selected studio logic

--- src/app/components/join-us/index.js
+++ src/app/components/join-us/index.js
@@ -6,7 +6,7 @@ import find from 'lodash/collection/find';
 import map from 'lodash/collection/map';
 import filter from 'lodash/collection/filter';
 import pluck from 'lodash/collection/pluck';
-import every from 'lodash/collection/every';
+import includes from 'lodash/collection/includes';
 import get from 'lodash/object/get';
 import kebabCase from 'lodash/string/kebabCase';
 import spannify from 'app/lib/spannify';
@@ -22,15 +22,10 @@ import Rimage from 'app/components/rimage';
 import Video from 'app/components/video';
 import Flux from 'app/flux';
 
-function isSelected(currentHash, id, studios) {
-  const studioNames = pluck(studios, 'name');
-  let selected = false;
-  if(
-    (!currentHash && id === 'all-studios')
-    || currentHash && id === 'all-studios' && every(studioNames, name => currentHash !== kebabCase(name))
-    || currentHash && currentHash === id
-  ) {
-    selected = true;
+function getSelectedStudio(studioSlugFromUrl, studioSlugs) {
+  let selected = 'all-studios';
+  if(includes(studioSlugs, studioSlugFromUrl)) {
+    selected = studioSlugFromUrl;
   }
   return selected;
 }
@@ -38,9 +33,12 @@ function isSelected(currentHash, id, studios) {
 const PageJoinUs = React.createClass({
   mixins: [getScrollTrackerMixin('join-us')],
   render() {
-    const { page } = this.props;
+    const { page, currentParams, studios } = this.props;
     const classes = classnames('page-join-us', this.props.className);
     const image = getFeaturedImage(page);
+    const studioSlugFromUrl = get(currentParams, 'lid');
+    const studioSlugs = map(pluck(studios, 'name'), kebabCase);
+    const selectedStudioSlug = getSelectedStudio(studioSlugFromUrl, studioSlugs);
 
     return <article className={classes}>
       <Hero
@@ -59,7 +57,7 @@ const PageJoinUs = React.createClass({
         colours: get(page, 'colors'),
         zebra: false,
         placeholderContents: {
-          WORKABLE_LIST: this.renderJobSection
+          WORKABLE_LIST: this.getJobSectionRenderer(selectedStudioSlug)
         }
       })}
     </article>;
@@ -69,16 +67,15 @@ const PageJoinUs = React.createClass({
       name: 'All studios'
     }].concat(this.props.studios);
   },
-  renderStudioTabs() {
-    const { currentParams } = this.props;
+  renderStudioTabs(selectedStudioSlug) {
     return map(this.getStudios(), studio => {
-      const studioId = kebabCase(studio.name);
+      const studioSlug = kebabCase(studio.name);
       const studioName = spannify(studio.name);
-      const uri = this.generateStudioUri(studioId);
+      const uri = this.generateStudioUri(studioSlug);
       return <li
-        key={`tab-${studioId}`}
-        className={studioId}
-        aria-selected={isSelected(get(currentParams, 'lid'), studioId, this.props.studios)}
+        key={`tab-${studioSlug}`}
+        className={studioSlug}
+        aria-selected={studioSlug === selectedStudioSlug}
       ><a href={uri} onClick={Flux.overrideNoScroll(uri)}>{studioName}</a></li>;
     });
   },
@@ -86,36 +83,37 @@ const PageJoinUs = React.createClass({
     const uri = studio !== 'all-studios' ? '/'+studio : '';
     return `/join-us${uri}`;
   },
-  renderJobSection() {
-    const sizes = { hardcoded: { url: '/images/joinus/current_openings.jpg' }};
+  getJobSectionRenderer(selectedStudioSlug) {
+    return () => {
+      const sizes = { hardcoded: { url: '/images/joinus/current_openings.jpg' }};
 
-    return <div>
-      <div className="current-openings">
-        <h2>Current Openings</h2>
-      </div>
-      <section className="jobs">
-        <nav className="jobs-studio-tabs">
-          {this.renderStudioTabs()}
-        </nav>
-        <div className="jobs-container">
-          {this.renderStudioJobs()}
+      return <div key="job-section">
+        <div className="current-openings">
+          <h2>Current Openings</h2>
         </div>
-      </section>
-      <div className="benefits">
-        <h2>Some of the benefits...</h2>
-      </div>
-    </div>;
+        <section className="jobs">
+          <nav className="jobs-studio-tabs">
+            {this.renderStudioTabs(selectedStudioSlug)}
+          </nav>
+          <div className="jobs-container">
+            {this.renderStudioJobs(selectedStudioSlug)}
+          </div>
+        </section>
+        <div className="benefits">
+          <h2>Some of the benefits...</h2>
+        </div>
+      </div>;
+    };
   },
-  renderStudioJobs() {
-    const { currentParams } = this.props;
+  renderStudioJobs(selectedStudioSlug) {
     return map(this.getStudios(), studio => {
-      const studioId = kebabCase(studio.name);
+      const studioSlug = kebabCase(studio.name);
       return <StudioJobs
-        key={`jobs-${studioId}`}
+        key={`jobs-${studioSlug}`}
         studio={studio}
         studios={this.props.studios}
         jobs={this.getJobsForStudio(studio)}
-        selected={isSelected(get(currentParams, 'lid'), studioId, this.props.studios)}
+        selected={studioSlug === selectedStudioSlug}
         contactEmail={get(find(get(find(get(this.props, 'footer.contacts', []), 'type', 'general'), 'methods', []), 'type', 'email'), 'uri', '')}
       />;
     });
