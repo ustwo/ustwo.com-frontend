411c93d5bbaa09ebac037f6aff75fcbf54ad1088
Phil Linnell <phil@ustwo.com>
2016-02-22 10:18:25 +0000

    Removed work-item-styles and case study now uses category-tag

--- src/app/components/work-item/index.js
+++ src/app/components/work-item/index.js
@@ -7,7 +7,6 @@ import Flux from 'app/flux';
 
 import Rimage from 'app/components/rimage';
 import ImageHover from 'app/components/image-hover';
-import WorkItemStyles from 'app/components/work-item-styles';
 import classnames from 'classnames';
 
 const WorkItem = React.createClass({
@@ -22,7 +21,6 @@ const WorkItem = React.createClass({
     const link = `/what-we-do/${get(data, 'slug')}`;
 
     return <div className={classnames('work-item', `work-item-${id}`)}>
-      <WorkItemStyles data={data} />
       <a href={link} onClick={Flux.override(link)} className="image">
         <Rimage
           wrap='div'
@@ -31,11 +29,11 @@ const WorkItem = React.createClass({
         />
         <ImageHover autoAnim={500} hover={this.state.hover} />
       </a>
-      <div className='details'>
-        <p className='type'>
+      <div className="details">
+        <div className="category-tag">
           {get(data, 'type')}
-        </p>
-        <h3 className='title'>
+        </div>
+        <h3 className="title">
           <a
             href={link}
             onMouseEnter={this.onMouseEnter}
@@ -44,12 +42,12 @@ const WorkItem = React.createClass({
           >{get(data, 'name')}</a>
         </h3>
         <div
-          className='desc'
+          className="desc"
           dangerouslySetInnerHTML={{__html: get(data, 'excerpt')}}
         />
         <div className="tail">
           <a
-            className='link'
+            className="link"
             href={link}
             onMouseEnter={this.onMouseEnter}
             onMouseLeave={this.onMouseLeave}
