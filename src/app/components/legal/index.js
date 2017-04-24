9d53ff2858364b0b077feb15c197ea778f3260a7
Phil Linnell <phil@ustwo.com>
2017-04-24 12:29:42 +0100

    fix: Legal page layout and background

--- src/app/components/legal/index.js
+++ src/app/components/legal/index.js
@@ -14,16 +14,17 @@ const PageLegal = React.createClass({
     const pageData = this.props.page;
     const sizes = { hardcoded: { url: '/images/photo.jpg' }};
 
-    return <article className="page-legal">
-      <Rimage className='hero-image' sizes={sizes} wrap='div' />
-      <div className="content-container">
-        <h2>Legal</h2>
-        <p>Ustwo Fampany Limited. Registered No. 05286528. Registered in England & Wales.</p>
-        <p>Registered office:</p>
-        <p>Unit G.01, Tea Building, 56 Shoreditch High Street, London, E1 6JJ</p>
-        <p>VAT number: 227 2497 00</p>
-      </div>
-    </article>;
+    return (
+      <article className="page-legal">
+        <div className="content-container">
+          <h2>Legal</h2>
+          <p>Ustwo Fampany Limited. Registered No. 05286528. Registered in England & Wales.</p>
+          <p>Registered office:</p>
+          <p>Unit G.01, Tea Building, 56 Shoreditch High Street, London, E1 6JJ</p>
+          <p>VAT number: 227 2497 00</p>
+        </div>
+      </article>
+    );
   }
 });
 
