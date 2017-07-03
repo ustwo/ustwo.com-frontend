604536dbef681a6870801c4e4dec30618b385f22
Phil Linnell <phil@ustwo.com>
2017-07-03 10:46:55 +0100

    feat: 404 page copy change + links added

--- src/app/components/404/index.js
+++ src/app/components/404/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Flux from 'app/flux';
 
 import getScrollTrackerMixin from 'app/lib/get-scroll-tracker-mixin';
 import Navigation from 'app/components/navigation';
@@ -10,7 +11,7 @@ const Page404 = React.createClass({
     return (
       <article className="page-404">
         <div className="page-404-large-text">404</div>
-        <p>Whoops nothing here</p>
+        <p>Whoops! There's a whole load of nothing here. <br />But no need to worry. Hit the <button onClick={Flux.override('/')}>home page</button>... or read something on the <button onClick={Flux.override('/blog')}>blog</button>?</p>
       </article>
     );
   }
