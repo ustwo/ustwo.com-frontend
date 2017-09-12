a1900bedbc379509a20e676bd5f8a01c393caf93
andyholland <andy@elementarydigital.co.uk>
2017-09-12 13:13:48 +0100

    Add frontend support for the new related posts module

--- src/app/components/case-study/index.js
+++ src/app/components/case-study/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
-import { get } from 'lodash';
+import {get} from 'lodash';
 import Meta from 'react-helmet';
 
 import getScrollTrackerMixin from 'app/lib/get-scroll-tracker-mixin';
@@ -11,73 +11,75 @@ import ContactBlock from 'app/components/contact-block';
 import Footer from 'app/components/footer';
 
 const PageCaseStudy = React.createClass({
-  mixins: [getScrollTrackerMixin('case-study')],
-  render() {
-    const { caseStudy, documentScrollPosition, viewportDimensions, footer, studios, currentPage } = this.props;
+    mixins: [getScrollTrackerMixin('case-study')],
+    render() {
+        const {caseStudy, documentScrollPosition, viewportDimensions, footer, studios, currentPage} = this.props;
 
-    let caseStudyName;
-    if (caseStudy && caseStudy.name === 'ustwo Auto') {
-      caseStudyName = 'ustwo-auto';
-    }
+        let caseStudyName;
+        if (caseStudy && caseStudy.name === 'ustwo Auto') {
+            caseStudyName = 'ustwo-auto';
+        }
 
-    const classes = classnames('page-case-study', this.props.className, caseStudyName);
+        const classes = classnames('page-case-study', this.props.className, caseStudyName);
 
-    return (
-      <article className={classes} key={`key-${caseStudyName}`}>
-        <Meta
-          title={get(caseStudy, 'seo.title') || ''}
-          meta={[{
-            name: "description",
-            content: get(caseStudy, 'seo.desc') || ''
-          }, {
-            name: "keywords",
-            content: get(caseStudy, 'seo.keywords') || ''
-          }, {
-            property: "og:type",
-            content: 'website'
-          }, {
-            property: "og:title",
-            content: get(caseStudy, 'seo.title') || ''
-          }, {
-            property: "og:description",
-            content: get(caseStudy, 'seo.desc') || ''
-          }, {
-            property: "og:image",
-            content: get(caseStudy, 'seo.image') || ''
-          }]}
-        />
-        <style>{`
+        return (
+            <article className={classes} key={`key-${caseStudyName}`}>
+                <Meta
+                    title={get(caseStudy, 'seo.title') || ''}
+                    meta={[{
+                        name: "description",
+                        content: get(caseStudy, 'seo.desc') || ''
+                    }, {
+                        name: "keywords",
+                        content: get(caseStudy, 'seo.keywords') || ''
+                    }, {
+                        property: "og:type",
+                        content: 'website'
+                    }, {
+                        property: "og:title",
+                        content: get(caseStudy, 'seo.title') || ''
+                    }, {
+                        property: "og:description",
+                        content: get(caseStudy, 'seo.desc') || ''
+                    }, {
+                        property: "og:image",
+                        content: get(caseStudy, 'seo.image') || ''
+                    }]}
+                />
+                <style>{`
           .page-case-study a {
             border-bottom-color: ${get(caseStudy, 'colors.secondary')};
           }
         `}</style>
-        <div className="page-content-wrapper">
-          {renderModules({
-            modules: get(caseStudy, 'page_builder', []),
-            colours: get(caseStudy, 'colors'),
-            zebra: true,
-            categories: get(caseStudy, 'categories')
-          })}
-          {this.renderRelatedContent()}
-        </div>
-        <ScrollWrapper
-          component={<ContactBlock />}
-          documentScrollPosition={documentScrollPosition}
-          viewportDimensions={viewportDimensions}
-          requireScreenPosition={true}
-          className="scroll-wrapper-contact-block"
-        />
-        <Footer data={footer} studios={studios} currentPage={currentPage}/>
-      </article>
-    );
-  },
-  renderRelatedContent() {
-    let relatedContent;
-    if(this.props.relatedContent.length) {
-      relatedContent = <RelatedContent content={this.props.relatedContent} />
+                <div className="page-content-wrapper">
+                    {renderModules({
+                        caseStudy: caseStudy,
+                        modules: get(caseStudy, 'page_builder', []),
+                        colours: get(caseStudy, 'colors'),
+                        zebra: true,
+                        categories: get(caseStudy, 'categories')
+                    })}
+                    {this.renderRelatedContent()}
+                </div>
+                <ScrollWrapper
+                    component={<ContactBlock />}
+                    documentScrollPosition={documentScrollPosition}
+                    viewportDimensions={viewportDimensions}
+                    requireScreenPosition={true}
+                    className="scroll-wrapper-contact-block"
+                />
+                <Footer data={footer} studios={studios} currentPage={currentPage}/>
+            </article>
+        );
+    },
+    renderRelatedContent() {
+        let relatedContent;
+
+        if (this.props.relatedContent.length) {
+            relatedContent = <RelatedContent content={this.props.relatedContent}/>
+        }
+        return relatedContent;
     }
-    return relatedContent;
-  }
 });
 
 export default PageCaseStudy;
