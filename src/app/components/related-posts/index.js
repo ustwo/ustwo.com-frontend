05caad29f6f5b9acdedd4dddc44666d0a7bec611
Phil Linnell <phil@ustwo.com>
2017-09-12 15:57:30 +0100

    refactor: Some small refactoring to related posts: spaces, pure function

--- src/app/components/related-posts/index.js
+++ src/app/components/related-posts/index.js
@@ -1,34 +1,30 @@
 import React from 'react';
-import {get} from 'lodash';
+import { get } from 'lodash';
 import WorkItem from 'app/components/work-item';
 import BlogPostListItem from 'app/components/blog-post-list-item';
 
-const RelatedPosts = React.createClass({
-
-    render() {
-
-        const renderItems = this.props.posts.map(data => {
-            let item;
-            if (data.type === 'post') {
-                item = <div className="related-content-item" key={data.id}><BlogPostListItem data={data}/></div>;
-            } else {
-                item = <div className="related-content-item" key={data.id}><WorkItem data={data}/></div>;
-            }
-            return item;
-        });
-
-
-        return (
-            <div className="related-content">
-                <div className="wrapper">
-                    <h2 className="more-juice-title">Related Posts</h2>
-                    <div className="related-content-cards">
-                        {renderItems}
-                    </div>
-                </div>
-            </div>
-        )
+function RelatedPosts({ posts }) {
+  
+  const renderItems = posts.map(data => {
+    let item;
+    if (data.type === 'post') {
+      item = <div className="related-content-item" key={data.id}><BlogPostListItem data={data}/></div>;
+    } else {
+      item = <div className="related-content-item" key={data.id}><WorkItem data={data}/></div>;
     }
-});
+    return item;
+  });
+
+  return (
+    <div className="related-content">
+      <div className="wrapper">
+        <h2 className="more-juice-title">Related Posts</h2>
+        <div className="related-content-cards">
+          {renderItems}
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default RelatedPosts;
