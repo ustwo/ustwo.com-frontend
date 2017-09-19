5ad0e491444582d3ec73488c6483176bb223ccee
Phil Linnell <phil@ustwo.com>
2017-09-19 17:53:11 +0100

    fix: Remove events from auto page, fix to previous commit

--- src/app/flux/routes.js
+++ src/app/flux/routes.js
@@ -61,13 +61,6 @@ const routes = {
       url: 'ustwo/v1/pages/auto',
       type: 'page',
       slug: 'auto'
-    }, {
-      url: 'ustwo/v1/events?per_page=5',
-      type: 'events',
-      slug: 'events'
-    }, {
-      url: 'ustwo/v1/events?per_page=3&archived=true',
-      type: 'archivedEvents'
     }]
   },
   humanisingAutonomy: {
